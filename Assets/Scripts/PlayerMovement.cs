1717742c60775a808f3f950a0de0c6c0b61c7010
CJHill <up2014114@myport.ac.uk>
2023-03-04 19:14:58 +0000

    Added Dash Mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f; // player movement speed
+<<<<<<< Updated upstream
      Vector3 movement;
+=======
+    public float dashDistance = 5f; // distance to dash
+    public float dashDuration = 0.2f; // duration of the dash
+    public float dashCooldown = 1f; // time to wait before dashing again
+    private float dashTimeRemaining = 0f; // time remaining for current dash
+    private float dashCooldownTimeRemaining = 0f; // time remaining for dash cooldown
+    Vector3 movement = new Vector3(0f, 0f, 0f);
+>>>>>>> Stashed changes
     // Update is called once per frame
     void Update()
     {
@@ -14,7 +23,11 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxisRaw("Vertical");
 
         // calculate the movement direction based on input
+<<<<<<< Updated upstream
         movement = new Vector3(0f, 0f, 0f);
+=======
+   
+>>>>>>> Stashed changes
         if (Input.GetKey(KeyCode.W))
         {
             movement += new Vector3(0f, 0f, 1f);
@@ -35,6 +48,38 @@ public class PlayerMovement : MonoBehaviour
 
         // move the player based on the movement direction and speed
         transform.position += movement * speed * Time.deltaTime;
-
+        HandleDash();
     }
+    void HandleDash()
+    {
+        // handle dash cooldown
+        if (dashCooldownTimeRemaining > 0)
+        {
+            dashCooldownTimeRemaining -= Time.deltaTime;
+        }
+
+        // handle dashing
+        if (dashTimeRemaining > 0)
+        {
+            Vector3 dashDirection = transform.forward;
+            transform.position += dashDirection * speed * Time.deltaTime;
+            dashTimeRemaining -= Time.deltaTime;
+        }
+        // check if player can dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && dashCooldownTimeRemaining <= 0 && movement.magnitude > 0)
+        {
+            // start dashing
+            dashTimeRemaining = dashDuration;
+            dashCooldownTimeRemaining = dashCooldown;
+
+            // calculate dash direction
+            Vector3 dashDirection = movement.normalized;
+            dashDirection.y = 0f;
+
+            // move player by dash distance
+            transform.position += dashDirection * dashDistance;
+        }
+    }
+
+        
 }
